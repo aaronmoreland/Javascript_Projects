84a4fa6a5b88da1c52d859b2f44a375e64fd21fa
Aaron Moreland <aaronmoreland@tx.rr.com>
2019-11-15 18:32:14 -0700

    Push Basic_Javascript Folder

---
+++ Basic_Javascript_Projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,6 @@
+var Family = "The Smith", Dad = "Mitch", Mom = "Donna", Son = "Collin" 
+Daughter = "Sarah", Dog = "Spot";
+var Family = Family.fontcolor("green");
+var Dad = Dad.fontcolor("blue");
+var Son = Son.fontcolor("red");
+document.write(Son)
